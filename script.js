c479b96c67e563ac328a7fcf33abb2377ff67925
ByteBigBoss <nethminas42@gmail.com>
2023-12-01 19:16:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function advacedSearch(){
+    window.location="advancedSearch.html";
+}
+function cart(){
+    window.location="cart.html";
+}
+function watchlist(){
+    window.location="watchlist.html";
+}
+function myOrders(){
+    window.location="myOrders.html";
+}
+function invoice(){
+    window.location="invoice.html";
+}
+function signup(){
+    window.location="signup.html";
+}
+function login(){
+    window.location="login.html";
+}
+function adminLogin(){
+    window.location="adminLogin.html";
+}
+function search(){
+    window.location="normalSearch.html";
+}
+function myshop(){
+    window.location="myShop-products.html";
+}
+function home(){
+    window.location="index.html";
+}
+function messages(){
+    window.location="messages.html";
+}
+function dashboard(){
+    window.location="adminaPanel-Dashboard.html";
+}
+function ManageUsers(){
+    window.location="adminPanel-ManageUsers.html";
+}
+function ManageProducts(){
+    window.location="adminPanel-ManageProducts.html";
+}
